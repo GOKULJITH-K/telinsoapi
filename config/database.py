a348441e866700d1874dfe18856b79d6ba2a0103
GOKULJITH K <87140397+GOKULJITH-K@users.noreply.github.com>
2024-01-31 18:36:37 +0530

    Add files via upload

---
+++ config/database.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://mongo:UNE8bmYvV38FARx5@cluster0.l2ubs1d.mongodb.net")
+
+db = client["login-tut"]
+
+collection_name = db["tests"]
