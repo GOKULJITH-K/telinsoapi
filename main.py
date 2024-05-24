cbf0ed1202939fb3a6dc35266a7e6108c778f3a3
GOKULJITH K <87140397+GOKULJITH-K@users.noreply.github.com>
2024-05-24 21:49:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from routes.route import router
-from sklearn.ensemble import RandomForestClassifier
-import pandas as pd
+
 
 # v8RcNc2VQL1D5p4m
 app = FastAPI()
@@ -12,27 +11,6 @@ app.include_router(router)
 
  
 
-dataset = pd.read_csv('crop_data.csv')
-
-
-X = dataset[['N', 'P', 'K', 'ph', 'humidity', 'ec', 'temperature']]
-y = dataset['Crop'] 
-print(dataset.columns) 
-
-rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
- 
-
-rf_classifier.fit(X, y)
-
-
-def predict_crop_success(N, P, K, ph, humidity, ec, temperature):
-    input_data = [[N, P, K, ph, humidity, ec, temperature]]
-    crop_prediction = rf_classifier.predict(input_data)[0]
-    crop_probabilities = rf_classifier.predict_proba(input_data)[0]
-    max_probability_index = crop_probabilities.argmax()
-    max_probability = crop_probabilities[max_probability_index]
-    suggested_crop = rf_classifier.classes_[max_probability_index]
-    return crop_prediction, max_probability * 100, suggested_crop
 
 
 
