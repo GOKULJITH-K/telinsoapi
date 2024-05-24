c1a40f5df2f338ccd1f3d8f85df2d53d88777a81
GOKULJITH K <87140397+GOKULJITH-K@users.noreply.github.com>
2024-05-24 21:49:00 +0530

    Update route.py

--- routes/route.py
+++ routes/route.py
@@ -4,6 +4,33 @@ from models.todos import CropData
 from config.database import collection_name
 from scheme.schemas import list_serial
 from bson import ObjectId
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+
+
+
+
+dataset = pd.read_csv('crop_data.csv')
+
+
+X = dataset[['N', 'P', 'K', 'ph', 'humidity', 'ec', 'temperature']]
+y = dataset['Crop'] 
+print(dataset.columns) 
+
+rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+ 
+
+rf_classifier.fit(X, y)
+
+
+def predict_crop_success(N, P, K, ph, humidity, ec, temperature):
+    input_data = [[N, P, K, ph, humidity, ec, temperature]]
+    crop_prediction = rf_classifier.predict(input_data)[0]
+    crop_probabilities = rf_classifier.predict_proba(input_data)[0]
+    max_probability_index = crop_probabilities.argmax()
+    max_probability = crop_probabilities[max_probability_index]
+    suggested_crop = rf_classifier.classes_[max_probability_index]
+    return crop_prediction, max_probability * 100, suggested_crop
 
 router = APIRouter()
 
