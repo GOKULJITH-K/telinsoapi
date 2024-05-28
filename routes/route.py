9986eb96582b79924cd2b81e4eadf15602ad00c3
GOKULJITH K <87140397+GOKULJITH-K@users.noreply.github.com>
2024-05-28 14:33:36 +0200

    Update route.py

--- routes/route.py
+++ routes/route.py
@@ -59,8 +59,7 @@ async def predict_crop_get(data:CropData):
     crop_prediction, success_percentage, suggested_crop = predict_crop_success(
         data.N, data.P, data.K, data.ph, data.humidity, data.ec, data.temperature
     )
-    return {
-        'crop_prediction': crop_prediction,
+    prediction ={'crop_prediction': crop_prediction,
         'success_percentage': success_percentage,
-        'suggested_crop': suggested_crop
-    }
+        'suggested_crop': suggested_crop}
+    return prediction
