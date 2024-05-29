5731d58a316a087aee03faa3d22674ea74edd8a2
GOKULJITH K <87140397+GOKULJITH-K@users.noreply.github.com>
2024-05-29 14:52:27 +0200

    Update schemas.py

--- scheme/schemas.py
+++ scheme/schemas.py
@@ -10,3 +10,16 @@ def individual_serial(todo) -> dict:
 
 def list_serial(todos) -> list:
     return [individual_serial(todo) for todo in todos]
+
+def individual_serial2(cropData) -> dict:
+    return {
+        "id": str(cropData["_id"]),
+        "crop_prediction": cropData["crop_prediction"],
+        "success_percentage": cropData["success_percentage"],
+        "suggested_crop": cropData["suggested_crop"],
+        "username":cropData["username"],
+        "__v": 0,
+    }
+
+def list_serial2(cropData) -> list:
+    return [individual_serial2(data) for data in cropData]
